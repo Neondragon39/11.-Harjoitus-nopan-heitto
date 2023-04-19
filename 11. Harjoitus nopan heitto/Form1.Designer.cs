e9ad9542eac0ed21e354418d598a07c3c1586d83
Neondragon39 <vlad.jeskin@gmail.com>
2023-04-19 06:21:06 +0300

    Add project files.

---
+++ 11. Harjoitus nopan heitto/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace _11._Harjoitus_nopan_heitto
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Noppa01PB = new System.Windows.Forms.PictureBox();
+            this.Noppa02PB = new System.Windows.Forms.PictureBox();
+            this.HeitaBT = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Noppa01PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Noppa02PB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(25, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nopan heitto";
+            // 
+            // Noppa01PB
+            // 
+            this.Noppa01PB.Location = new System.Drawing.Point(25, 73);
+            this.Noppa01PB.Name = "Noppa01PB";
+            this.Noppa01PB.Size = new System.Drawing.Size(100, 100);
+            this.Noppa01PB.TabIndex = 1;
+            this.Noppa01PB.TabStop = false;
+            // 
+            // Noppa02PB
+            // 
+            this.Noppa02PB.Location = new System.Drawing.Point(158, 73);
+            this.Noppa02PB.Name = "Noppa02PB";
+            this.Noppa02PB.Size = new System.Drawing.Size(100, 100);
+            this.Noppa02PB.TabIndex = 2;
+            this.Noppa02PB.TabStop = false;
+            // 
+            // HeitaBT
+            // 
+            this.HeitaBT.Font = new System.Drawing.Font("MV Boli", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.HeitaBT.Location = new System.Drawing.Point(29, 192);
+            this.HeitaBT.Name = "HeitaBT";
+            this.HeitaBT.Size = new System.Drawing.Size(229, 59);
+            this.HeitaBT.TabIndex = 3;
+            this.HeitaBT.Text = "Heitä";
+            this.HeitaBT.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 298);
+            this.Controls.Add(this.HeitaBT);
+            this.Controls.Add(this.Noppa02PB);
+            this.Controls.Add(this.Noppa01PB);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Noppa01PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Noppa02PB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox Noppa01PB;
+        private PictureBox Noppa02PB;
+        private Button HeitaBT;
+    }
+}
---
+++ 11. Harjoitus nopan heitto/Form1.cs
@@ -0,0 +1,10 @@
+namespace _11._Harjoitus_nopan_heitto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
